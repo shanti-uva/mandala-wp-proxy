398698a6de24762a9b5c396ecb638d813c0648f6
ThanGrove <than@virginia.edu>
2025-09-24 15:46:39 -0400

    Fixing TTT proxy path though not used, and adding generic JSON proxy path

--- mandala-proxy.php
+++ mandala-proxy.php
@@ -14,12 +14,16 @@ add_action('init', function () {
 
     // Solr proxy
     add_rewrite_rule('^proxy/solr/?$', 'index.php?solr_proxy=1', 'top');
+
+    // General proxy
+    add_rewrite_rule('^proxy/json/?$', 'index.php?json_proxy=1', 'top');
 });
 
 add_filter('query_vars', function ($vars) {
     $vars[] = 'geoserver_proxy';
     $vars[] = 'ttt_proxy';
     $vars[] = 'solr_proxy';
+    $vars[] = 'json_proxy';
     return $vars;
 });
 
@@ -58,20 +62,31 @@ add_action('template_redirect', function () {
     }
 
     if (get_query_var('ttt_proxy')) {
-        $target_url = 'https://ttt.thlib.org/'; // Replace with your target
+        $base_url = 'https://ttt.thlib.org/org.thdl.tib.scanner.RemoteScannerFilter';
 
-        // Get headers only
-        $headers = get_headers($target_url, true);
+        // Collect query string
+        $query_string = $_SERVER['QUERY_STRING'];
 
-        // Output as JSON
-        header('Content-Type: application/json');
-        echo json_encode($headers);
+        // Construct full target URL
+        $url = $base_url . '?' . $query_string;
+
+        $response = wp_remote_get($url, [
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+        ]);
+        $body = wp_remote_retrieve_body($response);
+        echo $body;
         exit;
     }
 
     if (get_query_var('solr_proxy')) {
         $base_url = 'https://texts.thdl.org/solr/select';
-
+        /*
+         * $base_url = 'http://host.docker.internal:8983/solr/thlcat/select';
+        if (strpos($_SERVER['SERVER_NAME'],'thlddev.ddev.site') !== FALSE) {
+            $base_url = 'http://solr1:8983/solr/thlcat/select';
+        }*/
         // Collect query string
         $query_string = $_SERVER['QUERY_STRING'];
 
@@ -102,6 +117,37 @@ add_action('template_redirect', function () {
         echo $body;
         exit;
     }
+
+    // General json proxy
+    if (get_query_var('json_proxy')) {
+        parse_str($_SERVER['QUERY_STRING'], $params);
+        $base_url = $params['url'];
+
+        // Use wp_remote_get to proxy the request
+        $response = wp_remote_get($base_url, [
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+            'sslverify' => false,
+        ]);
+
+        if (is_wp_error($response)) {
+            wp_send_json_error(['message' => 'Error fetching data from SOLR: ' . json_encode($response)]);
+        }
+
+        $body = wp_remote_retrieve_body($response);
+        $status = wp_remote_retrieve_response_code($response);
+
+        // Set CORS and content headers
+        header('Access-Control-Allow-Origin: *');// ✅ Add CORS headers
+        header('Access-Control-Allow-Methods: GET, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+        header('Content-Type: application/javascript; charset=UTF-8', true, $status);
+
+        echo $body;
+        exit;
+    }
+
 });
 
 register_activation_hook(__FILE__, function () {
